dfd16444a1973f6950e7d84a158b1a86bf5faf88
guneyumit <91600221+guneyumit@users.noreply.github.com>
2023-11-01 00:10:04 +0300

    Update Login_StepDefinitions.java

--- src/test/java/com/step_definitons/Login_StepDefinitions.java
+++ src/test/java/com/step_definitons/Login_StepDefinitions.java
@@ -10,44 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Login_StepDefinitions {
-    @When("user enters librarian username")
-    public void user_enters_librarian_username() {
-        System.out.println("user enters librarian username");
-    }
-    @When("user enters librarian password")
-    public void user_enters_librarian_password() {
-        System.out.println("uuser enters librarian password");
-
-    }
-    @Then("user should see the dashboard")
-    public void user_should_see_the_dashboard() {
-        System.out.println("user should see the dashboard");
-    }
-    @When("user enters student username")
-    public void user_enters_student_username() {
-        System.out.println("user enters student username");
-
-    }
-    @When("user enters student password")
-    public void user_enters_student_password() {
-        System.out.println("user enters student password");
-
-    }
-
-
-    @When("user enters admin username")
-    public void userEntersAdminUsername() {
-        System.out.println("user enters admin username");
-
-    }
-
-    @And("user enter admin password")
-    public void userEnterAdminPassword() {
-        System.out.println("user enter admin password");
-
-    }
-
-
 
     @Given("user is on the wikipedia page")
     public void userIsOnTheWikipediaPage() throws InterruptedException {
@@ -56,7 +18,7 @@ public class Login_StepDefinitions {
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://wikipedia.org");
-        String title = "Wikipedia";
+        String title = "Wikipedi";
 
         Assert.assertEquals("Page title NOT verified !!!", title, driver.getTitle());
 
